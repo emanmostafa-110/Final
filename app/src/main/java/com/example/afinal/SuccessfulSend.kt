ce384e9aaa31dcf5052b225056353e1a70aa6249
Admin <emanmostafaelnagar@gmail.com>
2022-07-02 03:16:09 +0200

    Eman3

---
+++ app/src/main/java/com/example/afinal/SuccessfulSend.kt
@@ -0,0 +1,23 @@
+package com.example.afinal
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import com.airbnb.lottie.LottieAnimationView
+
+class SuccessfulSend : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_successful_send)
+
+        val repasslottie =findViewById<LottieAnimationView>(R.id.repasslottie)
+
+        repasslottie.visibility = View.VISIBLE
+        Handler().postDelayed({
+            startActivity(Intent(this,Login::class.java))
+        },3000)
+
+    }
+}
